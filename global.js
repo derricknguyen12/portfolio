7d455c6aee545359844c825d65301d2830948eac
derricknguyen12 <derrick3600@yahoo.com>
2025-01-22 00:44:23 -0800

    Update ALL11

--- global.js
+++ global.js
@@ -5,10 +5,10 @@ function $$(selector) {
 }
 
 let pages = [
-    { url: 'index.html', title: 'Home' },
-    { url: 'contact/index.html', title: 'Contact' },
-    { url: 'resume/index.html', title: 'Resume' },
-    { url: 'projects/index.html', title: 'Projects' },
+    { url: '/', title: 'Home' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'resume/', title: 'Resume' },
+    { url: 'projects/', title: 'Projects' },
     { url: 'https://www.linkedin.com/in/derricknguyen12/', title: 'LinkedIn' },
     { url: 'https://github.com/derricknguyen12', title: 'GitHub' }
 ];
@@ -23,7 +23,7 @@ for (let p of pages) {
     let title = p.title;
 
     if (!url.startsWith('http')) {
-        url = !ARE_WE_HOME ? '../' + url : url;
+        url = !ARE_WE_HOME ? '../' + url : url; 
     }
 
     let a = document.createElement('a');
