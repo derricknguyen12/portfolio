bea414b9e480ba1c9d5ce50fafc1697ed5356d5d
derricknguyen12 <derrick3600@yahoo.com>
2025-02-12 15:53:34 -0800

    fix pages error

--- meta/main.js
+++ meta/main.js
@@ -1,22 +1,47 @@
 let data = [];
+let commits = [];
 
 async function loadData() {
-  data = await d3.csv('loc.csv');
-  console.log(data);
+    data = await d3.csv('../meta/loc.csv', (row) => ({
+        ...row,
+        line: Number(row.line),
+        depth: Number(row.depth),
+        length: Number(row.length),
+        date: new Date(row.date + 'T00:00' + row.timezone),
+        datetime: new Date(row.datetime),
+    }));
+    
+    console.log(data);
+    processCommits();
+    console.log(commits);
 }
 
-document.addEventListener('DOMContentLoaded', async () => {
-  await loadData();
-});
+function processCommits() {
+    commits = d3
+        .groups(data, (d) => d.commit)
+        .map(([commit, lines]) => {
+            let first = lines[0];
+            let { author, date, time, timezone, datetime } = first;
+            
+            let ret = {
+                id: commit,
+                url: 'https://github.com/YOUR_REPO/commit/' + commit,
+                author,
+                date,
+                time,
+                timezone,
+                datetime,
+                hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+                totalLines: lines.length,
+            };
 
-async function loadData() {
-    data = await d3.csv('loc.csv', (row) => ({
-      ...row,
-      line: Number(row.line),
-      depth: Number(row.depth),
-      length: Number(row.length),
-      date: new Date(row.date + 'T00:00' + row.timezone),
-      datetime: new Date(row.datetime),
-    }));
-  }
-
+            Object.defineProperty(ret, 'lines', {
+                value: lines,
+                configurable: false,
+                writable: false,
+                enumerable: false,
+            });
+            
+            return ret;
+        });
+}
